b5c9d13aac035368015ab460f4e88078766ca4bd
jul AMG <84226468+julmasked@users.noreply.github.com>
2024-02-09 22:45:58 +0000

    not-yet-finish

---
+++ main/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+// .change.bar1 {
+//     transform: translate(0, 11px) rotate(-45deg);
+// }
+
+// .change.bar2 { opacity: 0; }
+
+// .change.bar3 {
+//     transform: translate(0, -11px) rotate(45deg);
